e55f321cad6262d3a5784c8cb8be486223b83662
Man <sytect@hotmail.com>
2020-03-26 17:01:04 -0400

    adding jboss servlet-api dependency

---
+++ Deadline/src/main/java/Controller.java
@@ -0,0 +1,25 @@
+
+import view.StudentView;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author itzu
+ */
+public class Controller {
+    
+    private StudentView v;
+    
+    public Controller(StudentView v) {
+        v = v;
+    }
+    
+    public void initController() {
+    }
+    
+}
